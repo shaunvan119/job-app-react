b2dcbe201852d68c0a8638bdc8b7c2e086e674de
shaunvan119 <shaunvan@live.com.au>
2023-08-02 13:42:59 +1000

    updated theme and logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Box, Grid, Typography, Button } from '@material-ui/core'
+import { Box, Grid, Button } from '@material-ui/core'
+import companyLogo from "../../assets/logo.png"
+import "./header.css"
 
 
 
@@ -7,17 +9,18 @@ import { Box, Grid, Typography, Button } from '@material-ui/core'
 export default function  Header(props)  {
     
     return (
-<Box py={15} bgcolor="#572d80" color="white">
+<Box py={15} bgcolor="#A8A4E6" color="white">
     <Grid container justify="center">
     <Grid item xs={10}>
     <Box display="flex" justifyContent="space-between">
-    <Typography variant = "h5"> PT RECRUITMENT
-    </Typography>
+    <img className="logo" src={companyLogo} alt="BigCo Inc. logo"/>
     <Button
     onClick={props.openNewJobModel} 
     variant="contained" 
     color="primary" 
+    style={{ height: '50px' }}
     disableElevation>
+    
     Post a Job
     </Button>
     </Box>
